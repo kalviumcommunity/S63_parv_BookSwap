6dc10de8aef84355a8e5c46a3b5e75e42a070745
Parv Jhanwar <parvjhanwar@gmail.com>
2025-04-17 10:11:14 +0530

    Added POST API endpoint for adding a new book

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getAllBooks } = require("../controllers/bookController");
+const { getAllBooks, addBook } = require("../controllers/bookController");
 
-router.get("/books", getAllBooks);
+// Correct usage:
+router.get("/", getAllBooks);
+router.post("/", addBook);
 
 module.exports = router;
